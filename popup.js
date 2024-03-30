76eee69da3550fe407148e656860c29f7679f126
nauski <nauski@nauski.com>
2024-03-30 16:00:41 +0200

    cleanup

--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ const saveButton = document.getElementById('save');
   try {
     chrome.storage.local.set({downloadFolder}, () => {
     saveButton.style.borderColor = 'green';
-    chrome.storage.local.get(['downloadFolder'], (result) => {  // update the current folder input
+    chrome.storage.local.get(['downloadFolder'], (result) => {
       if (result.downloadFolder) {
 	  currentFolder.value = result.downloadFolder;
       }
